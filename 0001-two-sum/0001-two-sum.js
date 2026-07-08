bd22b863ed1d36233c5fec72c2f4965f622aadb1
Madhavi Porte <157514020+madhaviporte@users.noreply.github.com>
2026-07-08 14:53:32 +0530

    Time: 9 ms (42.04%), Space: 56.8 MB (11.4%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,12 +4,25 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i=0; i<nums.length; i++){
-        for(let j=i+1; j<nums.length; j++){
-            if(nums[i]+nums[j]=== target){
-                return [i,j]
-            }
-        }
+     let arr = nums.map((value, index) =>({
+        value,
+        index
+    }));
+    arr.sort((a,b) => a.value - b.value)
+
+     let left = 0
+     let right = arr.length-1
+   
+   while(left<right){
+    let sum = arr[left].value + arr[right].value
+    if(sum === target){
+        return [arr[left].index, arr[right].index]
+    }else if (sum<target){
+ left++
     }
+    else{
+        right--
+    }
+   }
     return []
 };
