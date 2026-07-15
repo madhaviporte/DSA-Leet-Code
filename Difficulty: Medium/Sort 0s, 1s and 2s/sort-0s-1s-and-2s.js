35258a186ab605ca8a8efe3741a494dfc68e44b2
Madhavi Porte <157514020+madhaviporte@users.noreply.github.com>
2026-07-15 08:23:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} arr
+ * @returns {void}
+ */
+
+class Solution {
+    sort012(arr) {
+        // code here
+        let low = 0;
+        let mid = 0;
+        let high = arr.length - 1;
+
+        while (mid <= high) {
+            if (arr[mid] === 0) {
+                [arr[low], arr[mid]] = [arr[mid], arr[low]];
+                low++;
+                mid++;
+            } 
+            else if (arr[mid] === 1) {
+                mid++;
+            } 
+            else {
+                [arr[mid], arr[high]] = [arr[high], arr[mid]];
+                high--;
+            }
+        }
+    }
+}
