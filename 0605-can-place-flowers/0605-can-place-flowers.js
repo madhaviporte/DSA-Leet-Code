4e3090dcd844250eac72bf3d344bda26ced8fe42
Madhavi Porte <157514020+madhaviporte@users.noreply.github.com>
2026-07-15 18:58:12 +0530

    Time: 3 ms (35.94%), Space: 56 MB (60.6%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+    let count =n;//reduce count if we are planting
+
+    for(let i =0; i<flowerbed.length; i++){
+        let curr= flowerbed[i];
+
+        if(curr==1) continue;
+        if(curr==0){
+//check the boundary value 1st :NOTE last and leftcheck/rightcheck will return boolean valuse
+            let leftcheck = (flowerbed[i-1]==0) || (i==0);
+            let rightcheck = (flowerbed[i+1]==0) || (i==flowerbed.length-1);
+//If i is the last index → no right neighbor → OK  //Otherwise → check actual neighbor i + 1
+
+            if(leftcheck && rightcheck){
+                count--;
+                flowerbed[i]=1;//mark as planted
+            }
+            if(count<=0) return true;//early exit if we've planted all needed flowers
+        }
+
+    }
+    return count<=0;
+};
