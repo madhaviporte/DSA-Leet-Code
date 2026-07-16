fcb8d4d5d52fd701004030fb294b7bb35ab8482a
Madhavi Porte <157514020+madhaviporte@users.noreply.github.com>
2026-07-16 22:33:00 +0530

    Time: 29 ms (75.8%), Space: 59 MB (54.34%) - LeetHub

---
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function(nums) {
+      nums.sort((a,b)=> b-a);
+    let l = nums.length;
+    let value1 = nums[0] * nums[1] * nums[2];
+    let value2 = nums[0] * nums[l-1] * nums[l-2];
+    return value1 > value2 ? value1 : value2;
+};
