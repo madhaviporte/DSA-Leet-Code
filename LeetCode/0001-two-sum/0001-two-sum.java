323e59d95d4bd042b6935679d800534ba55f4ee3
madhaviporte <madhaviporte2004@gmail.com>
2026-07-20 10:27:32 +0530

    .

--- LeetCode/0001-two-sum/0001-two-sum.java
+++ LeetCode/0001-two-sum/0001-two-sum.java
@@ -1,16 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-   Map<Integer, Integer> numMap = new HashMap<>();
-
-   for(int i=0; i<nums.length; i++){
-    int rem =  target - nums[i];
-
-    if(numMap.containsKey(rem)){
-        return new int[]{numMap.get(rem),i};
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
     }
-        numMap.put(nums[i],i);
-    
-   }
-   return new int[]{};
-    } 
 }
