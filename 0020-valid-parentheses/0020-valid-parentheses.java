6b307e5ccef0acb30e6f493158fbe13edf105e85
Madhavi Porte <157514020+madhaviporte@users.noreply.github.com>
2026-08-15 20:35:18 +0530

    Time: 5 ms (13.25%), Space: 43.7 MB (7.33%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        Map<Character, Character> smap = new HashMap<>();
+        Map<Character,Character> smap = new HashMap<>();
+        smap.put(')','(');
+        smap.put('}','{');
+        smap.put(']','[');
 
-        smap.put(')', '(');
-        smap.put('}', '{');
-        smap.put(']', '[');
-
-        for (char c : s.toCharArray()) {
-
-            if (smap.containsValue(c)) {
-                stack.push(c);
-            } 
-            else if (smap.containsKey(c)) {
-
-                if (stack.isEmpty() || smap.get(c) != stack.pop()) {
+        for(char c : s.toCharArray()){
+            if(smap.containsValue(c)){
+                 stack.push(c);
+            }
+            else if(smap.containsKey(c)){
+                if(stack.isEmpty() || smap.get(c) != stack.pop()){
                     return false;
                 }
             }
         }
-
         return stack.isEmpty();
     }
 }
