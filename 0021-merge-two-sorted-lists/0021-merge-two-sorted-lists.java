ae471a93962999456662e4218718daf06bc3d7f8
Madhavi Porte <157514020+madhaviporte@users.noreply.github.com>
2026-08-26 18:59:57 +0530

    Time: 0 ms (100%), Space: 44.3 MB (38.8%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1!=null & list2!=null){
+        if(list1 != null & list2 != null){
             if(list1.val<list2.val){
                 list1.next = mergeTwoLists(list1.next,list2);
                 return list1;
@@ -20,8 +20,11 @@ class Solution {
                 return list2;
             }
         }
-        if(list1==null)
-        return list2;
-        return list1; 
-    }
+       
+        
+         if(list1==null)
+            return list2;
+            return list1;
+    
+}
 }
