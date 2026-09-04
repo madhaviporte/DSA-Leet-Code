bbefd21d14ed562d845522a64d5b71bd81a57795
Madhavi Porte <157514020+madhaviporte@users.noreply.github.com>
2026-09-04 13:28:01 +0530

    Time: 1 ms (99.79%), Space: 46.3 MB (70.85%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,7 +1,6 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int n = nums.length;
-
         for(int i=0; i<n; i++){
             int maxValue = nums[i];
             int minValue = nums[i];
@@ -9,10 +8,11 @@ class Solution {
             for(int j=0; j<i; j++){
                 maxValue = Math.max(maxValue,nums[j]);
             }
+
             for(int j=i; j<n; j++){
                 minValue = Math.min(minValue,nums[j]);
             }
-            if(maxValue-minValue <=k){
+            if(maxValue-minValue <= k){
                 return i;
             }
         }
