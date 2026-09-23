8b807f5cfcf22c6af41ab3f6ac241e3c08e60914
Madhavi Porte <157514020+madhaviporte@users.noreply.github.com>
2026-09-23 23:49:10 +0530

    Time: 0 ms (100%), Space: 47.7 MB (13.89%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -3,10 +3,10 @@ class Solution {
         int n = nums.length;
         int xor = 0;
         for(int i=0; i<=n; i++){
-            xor ^= i;
+           xor^=i;
         }
         for(int num : nums){
-            xor ^= num;
+            xor^=num;
         }
         return xor;
     }
